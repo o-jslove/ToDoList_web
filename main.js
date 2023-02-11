3c5e09a4b60689055c32558e5d13647d2c029917
minseok Lee <gksflaxkdns12@gmail.com>
2023-02-11 17:08:52 +0900

    잠시 수정
    몇가지 기능 추가
    release 그대로

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 // + 버튼을 클릭하면, 할 일이 추가된다.
 // delete버튼을 누르면 할 일이 삭제된다.
 //check버튼을 누르면 할 일이 끝나면서 밑줄이 간다.
+//1. check 버튼을 클릭하는 순간 true false
+//2. true이면 끝난걸로 간주하고 밑줄 보여주기
+//3. false이면 안 끝난걸로 간주하고 그대로
+
 // 끝남 탭은 끝난 아이템만, 진행 탭은 진행 중인 아이템만
 // 전체 탭을 누르면 다시 전체 아이템으로 돌아온다.
 
@@ -12,32 +16,41 @@ addButton.addEventListener("click", addTask);
 taskInput.addEventListener("focus", function() {
     taskInput.value = "";
 })
-taskInput.addEventListener("keyup", function(event) {
-    if(event.keyCode === 13) {
-        event.preventDefault();
-    }
-})
 
 
-// 함수 Area
+
+// 함수 Area-----------------------------
+
+//할 일 추가하는 함수
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+
+    //객체 생성하기
+    let task = {
+        taskContent: taskInput.value,
+        isComplete: false
+    }
+
+    taskList.push(task);
     console.log(taskList);
     render();
 }
 
+// 랜더링하기
 function render() {
     let resultHTML = '';
     for(let i=0; i<taskList.length; i++) {
         resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
+        <div>${taskList[i].taskContent}</div>
         <div>
-            <button>check</button>
-            <button>delete</button>
+            <button onclick="toggleComplete()">check</button>
+            <button id="">delete</button>
         </div>
     </div>`
     }
 
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete() {
+    
 }
